6acb23b8fc65345257e8a8369ce9c32e86dbb056
Minaish Rustomji <minaishrustomji@Minaishs-MacBook-Air.local>
2026-02-10 11:37:37 +0530

    files merged

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #Calculator
+
+import addition as a
+import subtraction as s
+import multiplication as m
+
+print("Addition Result = ",a.add(5,6))
+print("Subtraction Result = ",s.sub(5,3))
+print("Multiplication Result = ",m.mul(2,3))
+
+
